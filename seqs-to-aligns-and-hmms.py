e69ac4a883fe052db40384b4375f6943c3d9e715
Brian Osborne <briano@bioteam.net>
2020-04-14 22:27:40 -0400

    Working  version

--- seqs-to-aligns-and-hmms.py
+++ seqs-to-aligns-and-hmms.py
@@ -4,50 +4,46 @@ import argparse
 import os.path
 import yaml
 import tempfile
+import subprocess
 from Bio import SeqIO
-# from Bio.Seq import Seq
-# from Bio.SeqRecord import SeqRecord
-# from Bio.Alphabet import IUPAC
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('-f', default='fasta', dest='format', help="Output format")
-# parser.add_argument('-split', default=False, dest='split', help="Split into separate files")
-# parser.add_argument('-no-split', action='store_false', dest='split', help="Create one file")
 parser.add_argument('-analyze', default=False, action='store_true', help="Parse files without file creation")
 parser.add_argument('-verbose', default=False, action='store_true', help="Verbose")
 parser.add_argument('-aligner', default='muscle', help="Alignment application")
+parser.add_argument('-hmmbuild', default='hmmbuild', help="HMM build application")
 parser.add_argument('files', nargs='+', help='File names')
 args = parser.parse_args()
 
 
 def main():
-    builder = Seqs_To_Aligns_And_Hmms(args.analyze, args.verbose, args.aligner, args.files)
+    builder = Seqs_To_Aligns_And_Hmms(args.analyze, args.verbose, args.aligner, args.hmmbuild, args.files)
     builder.read()
-    builder.align()
-    # builder.create_objects()
-    # builder.write()
-
+    builder.make_align()
+    builder.make_hmm()
 
 class Seqs_To_Aligns_And_Hmms:
 
-    def __init__(self, analyze, verbose, aligner, files):
-        # self.seq_format = seq_format
+    def __init__(self, analyze, verbose, aligner, hmmbuild, files):
         self.analyze = analyze
         self.verbose = verbose
         self.aligner = aligner
+        self.hmmbuild = hmmbuild
         self.files = files
         self.seqs = dict()
+        self.alns = dict()
+        self.hmms = dict()
 
     def read(self):
         full_paths = [os.path.join(os.getcwd(), path) for path in self.files]
         for path in full_paths:
             seqs = []
+            name = os.path.basename(path).split('.')[0]
             try:
                 # Each one of these is a multiple fasta file
                 for index, fa in enumerate(SeqIO.parse(path,"fasta")):
                     seqs.append(fa)
-                seqs = self.remove_dups(seqs)
-                self.seqs[os.path.basename(path)] = seqs
+                self.seqs[name] = self.remove_dups(seqs)
             except (RuntimeError) as exception:
                 print("Error parsing sequences in '" +
                       str(path) + "':" + str(exception))
@@ -58,43 +54,19 @@ class Seqs_To_Aligns_And_Hmms:
             d[str(seq.seq)] = seq
         return list(d.values())
         
-    def align(self):
-        for filename in self.seqs:
-            # print("{}".format(self.seqs[filename]))
-            fp = tempfile.TemporaryFile()
-            SeqIO.write(self.seqs[filename], fp, 'fasta')
-   
-        # self.get_host()
-        # self.get_date()
-        # self.get_organism()
-        # self.standardize_cds()
-        # self.standardize_mat_peptide()
+    def make_align(self):
+        for name in self.seqs:
+            seqfile = tempfile.NamedTemporaryFile('w')
+            SeqIO.write(self.seqs[name], seqfile, 'fasta')
+            subprocess.run([self.aligner, '-in', seqfile.name, '-out', name + '.aln'])
+            self.alns[name] = name + '.aln'
 
-    def create_objects(self):
-        if self.analyze:
-            return
-        '''
-        '''
-
-
-    def write(self):
-        if self.analyze:
-            return
-        if self.split:
-            return
-            # for acc in self.accs:
-            #     for feat in self.accs[acc]:
-            #         seqfile = acc + '.' + self.seq_format
-            # SeqIO.write(record, seqfile, self.seq_format)
-            # seqfile = record + '-CDS' + '.' + self.seq_format
-            # SeqIO.write(seqs, seqfile, self.seq_format)
-        else:
-            for name in self.aa.keys():
-                seqfile = name + '-aa.' + self.seq_format
-                SeqIO.write(self.aa[name], seqfile, self.seq_format)
-            for name in self.nt.keys():
-                seqfile = name + '-nt.' + self.seq_format
-                SeqIO.write(self.nt[name], seqfile, self.seq_format)
+    def make_hmm(self):
+        for name in self.alns:
+            # Either --amino or --dna
+            opt = '--amino' if '-aa' in name else '--dna'
+            subprocess.run([self.hmmbuild, opt, name + '.hmm', self.alns[name]])
+            self.hmms[name] = name + '.hmm'
 
 if __name__ == "__main__":
     main()
