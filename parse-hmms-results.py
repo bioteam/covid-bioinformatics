069bce677088041e15054d1e163303ae4054a263
Brian Osborne <briano@bioteam.net>
2020-05-01 07:56:27 -0400

    Update COV vocabulary and gene variants to remove stray sequences

--- parse-hmms-results.py
+++ parse-hmms-results.py
@@ -8,35 +8,38 @@ from Bio import SearchIO
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-verbose', default=False,
-                    action='store_true', help="Verbose")
+parser.add_argument('-verbose', default=False, action='store_true', help="Verbose")
+parser.add_argument('-mismatch', default=False, action='store_true', help="Find mismatched")
 parser.add_argument('files', nargs='+', help='File names')
 args = parser.parse_args()
 
 
 def main():
-    builder = Parse_Hmms_Results(args.verbose, args.files)
+    builder = Parse_Hmms_Results(args.verbose, args.files, args.mismatch)
     builder.read()
 
 
 class Parse_Hmms_Results:
 
-    def __init__(self, verbose, files):
+    def __init__(self, verbose, files, mismatch):
         self.verbose = verbose
         self.files = files
+        self.mismatch = mismatch
 
     def read(self):
-        # full_paths = [os.path.join(os.getcwd(), path) for path in self.files]
         for file in self.files:
-            matches = re.match(r'(\w+-\w+)_(\w+-\w+)', file)
+            matches = re.match(r'(\w+-\w+)_(\w+-\w+)', os.path.basename(file))
             # Turn a Generator into a list
             qresult = list(SearchIO.parse(file, 'hmmer3-tab'))
             if not qresult:
                 print("No match:\t{0}\t{1}".format(matches[1], matches[2]))
+                continue
+            hit = qresult[0].hits[0]
+            if self.mismatch:
+                if qresult[0].id.split('-')[0] != hit.id.split('-')[0]:
+                    print("{0}\t{1}\t{2}".format(qresult[0].id, hit.id, hit.evalue))
             else:
-                print("{0}\t{1}\t{2}".format(qresult[0].id, qresult[0].accession, len(qresult[0].hits)))
-                hit = qresult[0].hits[0]
-                print("{0}\t{1}".format(hit.id, hit.evalue))
+                print("{0}\t{1}\t{2}".format(qresult[0].id, hit.id, hit.evalue))
 
 
 if __name__ == "__main__":
