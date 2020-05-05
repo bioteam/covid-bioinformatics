ab68586fcb8d63f67df531ebd8788ebbd8e47f84
Brian Osborne <bosborne@c100-002.frontera.tacc.utexas.edu>
2020-05-05 08:51:30 -0500

    Initial script for tax analysis

---
+++ setup-taxdb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+from ete2 import NCBITaxa
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-verbose', default=False, action='store_true', help="Verbose")
+
+
+def main():
+    builder = Setup_Taxdb(args.verbose)
+
+
+class Sequp_Taxdb:
+
+    def __init__(self, verbose):
+        self.verbose = verbose
+	ncbi = NCBITaxa()
+
+if __name__ == "__main__":
+    main()
