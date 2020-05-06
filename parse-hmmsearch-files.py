3ddaeb2c1222a3c9f61819602f92b9dd7692bf13
Brian Osborne <briano@bioteam.net>
2020-05-06 17:36:03 -0400

    In progress

--- parse-hmmsearch-files.py
+++ parse-hmmsearch-files.py
@@ -9,7 +9,7 @@ from Bio import SearchIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-verbose', default=False, action='store_true', help="Verbose")
-parser.add_argument('-db', default='    ', help="Database")
+parser.add_argument('-db', default='', help="Database")
 parser.add_argument('files', nargs='+', help='File names')
 args = parser.parse_args()
 
@@ -24,11 +24,11 @@ class Query_Taxadb:
         self.verbose = verbose
         self.files = files
         self.dbpath = os.path.join(Path.home(),'taxonomy','taxadb.sqlite')
-        self.taxdb = TaxID(dbtype='sqlite', dbname=self.dbpath)
+        self.taxid = TaxID(dbtype='sqlite', dbname=self.dbpath)
         self.accession = AccessionID(dbtype='sqlite', dbname=self.dbpath)
  
  
-    def read:
+    def read(self):
         for file in self.files:
             matches = re.match(r'(\w+-\w+)_(\w+-\w+)', os.path.basename(file))
             # Turn a Generator into a list
@@ -37,8 +37,8 @@ class Query_Taxadb:
                 print("No match:\t{0}\t{1}".format(matches[1], matches[2]))
                 continue
             hit = qresult[0].hits[0]
-            taxids = accession.taxid(hit.id)
-            lineage = taxid.lineage_name(taxid, reverse=True)
+            taxids = self.accession.taxid(hit.id)
+            lineage = self.taxid.lineage_name(taxid, reverse=True)
 
             #     if qresult[0].id.split('-')[0] != hit.id.split('-')[0]:
             #         print("{0}\t{1}\t{2}".format(qresult[0].id, hit.id, hit.evalue))
