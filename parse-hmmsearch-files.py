0f76bd3f5c00f462e8e84e0e7b35a5dba5cd62d4
Brian Osborne <briano@bioteam.net>
2020-05-09 15:49:27 -0400

    Filter option works

--- parse-hmmsearch-files.py
+++ parse-hmmsearch-files.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python3
 
 import argparse
-from pathlib import Path
-import os
-import re
 from Bio import SearchIO
 from Bio import Entrez
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-verbose', default=False, action='store_true', help="Verbose")
-parser.add_argument('-taxfilter', help="Exclude")
+parser.add_argument('-download', default=False, action='store_true', help="Download hit sequences")
+parser.add_argument('-align', default=False, action='store_true', help="Align hits to HMM")
+parser.add_argument('-taxfilter', default=None, help="Exclude clade")
 parser.add_argument('files', nargs='+', help='File names')
 args = parser.parse_args()
 
 
 def main():
-    query = Parse_Hmmsearch(args.verbose, args.taxfilter, args.files)
-    query.read()
+    query = Parse_Hmmsearch(args.verbose, args.download, args.align, args.taxfilter, args.files)
+    query.parse()
+    query.filter()
+    query.download_hits()
+    query.align_hits_to_hmm()
 
 class Parse_Hmmsearch:
 
-    def __init__(self, verbose, taxfilter, files):
+    def __init__(self, verbose, download, align, taxfilter, files):
         self.verbose = verbose
+        self.download = download
+        self.align = align
         self.taxfilter = taxfilter
         self.files = files
+        self.hits = dict()
         self.email = 'briano@bioteam.net'
  
-    def read(self):
+    def parse(self):
         for file in self.files:
             matches = re.match(r'(\w+-\w+)_(\w+-\w+)', os.path.basename(file))
             for qresult in SearchIO.parse(file, 'hmmer3-tab'):
@@ -36,11 +41,9 @@ class Parse_Hmmsearch:
                     continue
                 for hit in qresult:
                     taxid = self.get_taxid(hit.id)
-                    if self.verbose:
-                        print("Hit taxonomy id: {}".format(taxid))
-                    lineage = self.get_lineage(taxid)
-                    if self.verbose and lineage:
-                        print("Hit lineage: {}".format(lineage))
+                    # if self.verbose:
+                    #     print("Hit taxonomy id: {}".format(taxid))
+                    self.hits[hit.id] = self.get_lineage(taxid)
 
 
     def get_taxid(self, pid):
@@ -51,15 +54,44 @@ class Parse_Hmmsearch:
         taxid = result[0]["LinkSetDb"][0]["Link"][0]["Id"]
         return taxid
 
+    def download_hits(self):
+        if not self.download:
+            return
+        try:
+            if self.verbose:
+                print("Downloading records: {}".format(id_chunk))
+            handle = Entrez.efetch(
+                    db="protein",
+                    rettype='fasta',
+                    retmode="text",
+                    id=','.join(self.hits.keys())
+            )
+            self.fasta = handle.read()
+            handle.close()
+        except (RuntimeError) as exception:
+            print("Error retrieving sequences using id '" +
+                  str(pids) + "':" + str(exception))
+
+    def align_hits_to_hmm(self):
+        if not self.align:
+            return
+
+    def filter(self):
+        if self.taxfilter:
+            filtered = dict()
+            for hit in self.hits:
+                if self.taxfilter not in self.hits[hit]:
+                    filtered[hit] = self.hits[hit]
+            self.hits = filtered
+        if self.verbose:
+            for hit in self.hits:
+                print("Lineage is: {}".format(self.hits[hit]))
 
     def get_lineage(self, taxid):
         Entrez.email = self.email
         handle = Entrez.efetch(db="taxonomy", id=taxid)
         records = Entrez.read(handle)
-        # With "LineageEx" you’ll get the NCBI taxon identifiers of the lineage entries
-        if self.taxfilter:
-            if self.taxfilter in records[0]["Lineage"]:
-                return None
+        # With "LineageEx" you get the NCBI taxon identifiers of the clades
         return records[0]["Lineage"]
  
 
