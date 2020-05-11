080471a284f3fc5737b0f6084e4a6ccf2bbc12f3
Brian Osborne <briano@bioteam.net>
2020-05-11 10:25:00 -0400

    Add to setup

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 requirements = ['biopython', 'numpy', 'PyYAML']
 
 setup(
-    name='covid_bioinformatics',
+    name='covid_bio',
     use_scm_version=True,
     author="Brian Osborne",
     author_email="briano@bioteam.net",
     description="Tools for COV sequence analysis",
-    packages=['covid_bioinformatics'],
+    packages=['covid_bio'],
+    entry_points={ 
+    'console_scripts': ['covidbiotool=covid_bio.cli:main']
+    },
     python_requires='>=3.7',
     setup_requires=['setuptools_scm'],
     install_requires=requirements,
