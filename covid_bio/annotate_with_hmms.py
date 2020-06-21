1e133733328bd702d57c373eb8d7b91048a242c2
Brian Osborne <briano@bioteam.net>
2020-06-21 13:37:55 -0400

    Working code, creates BED

--- covid_bio/annotate_with_hmms.py
+++ covid_bio/annotate_with_hmms.py
@@ -4,46 +4,54 @@ import argparse
 import sys
 import os
 import glob
-# import re
-# import tempfile
+import tempfile
 import subprocess
 from Bio import SearchIO
+from Bio import SeqIO
 
 '''
-BED Format (https://genome.ucsc.edu/FAQ/FAQformat.html#format1)
+BED Format (https://m.ensembl.org/info/website/upload/bed.html)
 
-The first three required BED fields are:
+Required fields
+The first three fields in each feature line are required:
 
-chrom - The name of the chromosome (e.g. chr3, chrY, chr2_random) or scaffold (e.g. scaffold10671).
-chromStart - The starting position of the feature in the chromosome or scaffold. The first base in a chromosome is numbered 0.
-chromEnd - The ending position of the feature in the chromosome or scaffold. The chromEnd base is not included in the display of the 
-feature, however, the number in position format will be represented. For example, the first 100 bases of chromosome 1 are defined as 
-chrom=1, chromStart=0, chromEnd=100, and span the bases numbered 0-99 in our software (not 0-100), but will represent the position 
-notation chr1:1-100.
+chrom - name of the chromosome or scaffold. Any valid seq_region_name can be used, with or without the 'chr' prefix.
+chromStart - Start position of the feature in standard chromosomal coordinates (i.e. first base is 0).
+chromEnd - End position of the feature in standard chromosomal coordinates
 
-The 9 additional optional BED fields are:
+Optional fields
+Nine additional fields are optional. Note that columns cannot be empty - lower-numbered fields must always be populated if higher-numbered ones are used.
 
-name - Defines the name of the BED line. 
-score - A score between 0 and 1000.
-strand - Defines the strand. Either "." (=no strand) or "+" or "-".
-thickStart - The starting position at which the feature is drawn thickly (for example, the start codon in gene displays). When there 
-is no thick part, thickStart and thickEnd are usually set to the chromStart position.
-thickEnd - The ending position at which the feature is drawn thickly (for example the stop codon in gene displays).
-itemRgb - An RGB value of the form R,G,B (e.g. 255,0,0). If the track line itemRgb attribute is set to "On", this RBG value will 
-determine the display color of the data contained in this BED line. NOTE: It is recommended that a simple color scheme (eight colors 
-or less) be used with this attribute to avoid overwhelming the color resources of the Genome Browser and your Internet browser.
-blockCount - The number of blocks (exons) in the BED line.
-blockSizes - A comma-separated list of the block sizes. The number of items in this list should correspond to blockCount.
-blockStarts - A comma-separated list of block starts. All of the blockStart positions should be calculated relative to chromStart. 
-The number of items in this list should correspond to blockCount.
-In BED files with block definitions, the first blockStart value must be 0, so that the first block begins at chromStart. Similarly, 
-the final blockStart position plus the final blockSize value must equal chromEnd. Blocks may not overlap.
+name - Label to be displayed under the feature, if turned on in "Configure this page".
+score - A score between 0 and 1000. See track lines, below, for ways to configure the display style of scored data.
+strand - defined as + (forward) or - (reverse).
+thickStart - coordinate at which to start drawing the feature as a solid rectangle
+thickEnd - coordinate at which to stop drawing the feature as a solid rectangle
+itemRgb - an RGB colour value (e.g. 0,0,255). Only used if there is a track line with the value of itemRgb set to "on" (case-insensitive).
+blockCount - the number of sub-elements (e.g. exons) within the feature
+blockSizes - the size of these sub-elements
+blockStarts - the start coordinate of each sub-element
 
-Here's an example of an annotation track, introduced by a header line:
+Track lines
 
-track name=pairedReads description="Clone Paired Reads" useScore=1
-chr22 1000 5000 cloneA 960 + 1000 5000 0 2 567,488, 0,3512
-chr22 2000 6000 cloneB 900 - 2000 6000 0 2 433,399, 0,3601
+Track definition lines can be used to configure the display further, e.g. by grouping features into separate tracks. 
+Track lines should be placed at the beginning of the list of features they are to affect.
+
+The track line consists of the word 'track' followed by space-separated key=value pairs - see the example below. 
+Valid parameters used are:
+
+name - unique name to identify this track when parsing the file
+description - Label to be displayed under the track in Region in Detail
+priority - integer defining the order in which to display tracks, if multiple tracks are defined.
+color - as RGB, hex or X11 named color.
+useScore - set to 1 to render the track in greyscale based on the values in the score column.
+itemRgb - if set to 'on' (case-insensitive), the individual RGB values defined in tracks will be used.
+
+track name="ItemRGBDemo" description="Item RGB demonstration" itemRgb="On"
+chr7  127471196  127472363  Pos1  0  +  127471196  127472363  255,0,0
+chr7  127472363  127473530  Pos2  0  +  127472363  127473530  255,0,0
+chr7  127475864  127477031  Neg1  0  -  127475864  127477031  0,0,255
+chr7  127477031  127478198  Neg2  0  -  127477031  127478198  0,0,255
 '''
 
 parser = argparse.ArgumentParser()
@@ -57,6 +65,7 @@ def main():
     annotator = Annotate_With_Hmms(args.verbose, args.hmmdir, args.files)
     annotator.get_hmms()
     annotator.annotate()
+    annotator.write()
 
 
 class Annotate_With_Hmms:
@@ -64,95 +73,90 @@ class Annotate_With_Hmms:
         self.verbose = verbose
         self.hmmdir = hmmdir
         self.files = files
+        self.bed = dict()
+
 
     def annotate(self):
         for file in self.files:
-            name = os.path.basename(file).split('.')[0]
-            if self.verbose:
-                print("Annotating {}".format(name))
+            name = self.write_fasta(file)
+            self.bed[name] = []
+            self.bed[name].append(['track name="' + name + '"',
+                'description="HMM-based annotation of COV sequence ' + name + '"',
+                'itemRgb="on"'])
+            for hmm in self.hmms:
+                feat = os.path.basename(hmm).split('-')[0]
+                if self.verbose:
+                    print("Annotating {0} with {1}".format(name,hmm))
+                hit = self.run_hmmsearch(name,hmm)
+                # Create feature line
+                featureline = []
+                featureline.append(str(1))
+                featureline.append(str(hit.hit_start - 1))
+                featureline.append(str(hit.hit_end))
+                featureline.append(feat)
+                featureline.append(str(hit.bitscore))
+                featureline.append('+')
+                # If there is an ATG
+                thickStart = str(hit.hit_start - 1) if 'ORF' in feat or feat in 'EMNS' else ''
+                featureline.append(thickStart)
+                thickEnd = str(hit.hit_start + 2) if 'ORF' in feat or feat in 'EMNS' else ''
+                featureline.append(thickEnd)
+                # Color features
+                if feat in 'EMNS':
+                    featureline.append('66,245,173')
+                elif 'ORF' in feat:
+                    featureline.append('66,123,245')
+                else:
+                    featureline.append('245,66,197')
+                self.bed[name].append(featureline)
+
 
     def get_hmms(self):
-        self.hmms = [f for f in glob.glob(
-            self.hmmdir + '/*nt.hmm', recursive=False)]
+        # Get all nucleotide HMMs
+        self.hmms = [f for f in glob.glob(self.hmmdir + '/*nt.hmm', recursive=False)]
         if self.verbose:
             print("HMMs: {}".format(self.hmms))
 
-    def read(self):
-        for path in [f for f in self.files if os.path.isfile(f)]:
-            seqs = []
-            # Get basename without suffix, for example "S-aa"
-            name = os.path.basename(path).split('.')[0]
-            try:
-                if self.verbose:
-                    print("Reading Fasta file: {}".format(path))
-                # Each one of these is a multiple fasta file
-                for index, fa in enumerate(SeqIO.parse(path, "fasta")):
-                    seqs.append(fa)
-                self.seqs[name] = self.remove_dups(seqs)
-            except (RuntimeError) as exception:
-                print("Error parsing sequences in '" +
-                      str(path) + "':" + str(exception))
 
-    def make_align(self):
-        for name in self.seqs:
-            # Most aligners will reject a file with a single sequence so just copy
-            if len(self.seqs[name]) == 1:
-                cmd = ['cp', name + '.fa', name + '.fasta']
-                subprocess.run(cmd, check=True)
-            else:
-                seqfile = tempfile.NamedTemporaryFile('w', delete=False)
-                SeqIO.write(self.seqs[name], seqfile.name, 'fasta')
-                if self.verbose:
-                    print("Alignment input sequence file: {}".format(seqfile.name))
-                # out_filename is used to redirect the STDOUT to file
-                # when self.aligner is "mafft" and it requires redirect to file
-                cmd, out_filename = self.make_align_cmd(seqfile.name, name)
-                if self.verbose:
-                    print("Alignment command is '{}'".format(cmd))
-                try:
-                    if out_filename:
-                        with open(out_filename, "w") as f:
-                            subprocess.run(cmd, check=True, stdout=f)
-                    else:
-                        subprocess.run(cmd, check=True)
-                except (subprocess.CalledProcessError) as exception:
-                    print("Error running '{}': ".format(
-                        self.aligner) + str(exception))
+    def write_fasta(self, file):
+        name = os.path.basename(file).split('.')[0]
+        gb = SeqIO.parse(file, 'gb')
+        # Create fasta "reference" sequence
+        SeqIO.write(gb, name + '.fa', 'fasta')
+        # Index with samtools
+        cmd = ['samtools', 'faidx', name + '.fa']
+        try:
+            subprocess.run(cmd, check=True)
+        except (subprocess.CalledProcessError) as exception:
+            print("Error: {}".format(exception))
+            sys.exit("Error running samtools")
+        return name
 
-            # Create additional Maf format alignment
-            if self.maf:
-                AlignIO.convert(name + '.fasta', 'fasta',
-                                name + '.maf', 'maf', alphabet=None)
 
-            self.alns[name] = name + '.fasta'
+    def run_hmmsearch(self,name,hmm):
+        out = tempfile.NamedTemporaryFile('w')
+        cmd = ['hmmsearch', '--noali', '-o', out.name, hmm, name + '.fa']
+        try:
+            subprocess.run(cmd, check=True)
+        except (subprocess.CalledProcessError) as exception:
+            print("Error: {}".format(exception))
+            sys.exit("Error running hmmsearch")
+        bestscore = 0
+        # Get HSP with highest score
+        for qresult in SearchIO.parse(out.name, 'hmmer3-text'):
+            for hit in qresult:
+                for hsp in hit:
+                    if hsp.bitscore > bestscore:
+                        besthit = hsp
+                        bestscore = hsp.bitscore
+        return besthit
 
-    def make_align_cmd(self, infile, name):
-        '''
-        '''
-        if self.aligner == 'muscle':
-            return [self.aligner, '-quiet', '-in', infile, '-out', name + '.fasta'], None
-        elif self.aligner == 'mafft':
-            return [self.aligner, '--auto', infile], name + '.fasta'
-        elif self.aligner == 'clustalo':
-            return [self.aligner, '-i', infile, '-o', name + '.fasta', '--outfmt=fasta'], None
-        else:
-            sys.exit("No command for aligner {}".format(self.aligner))
 
-    def make_hmm(self):
-        for name in self.alns:
-            if self.verbose:
-                print("{0} input file is '{1}'".format(
-                    self.hmmbuild, self.alns[name]))
-            # Either --amino or --dna
-            opt = '--amino' if '-aa' in name else '--dna'
-            try:
-                subprocess.run(
-                    [self.hmmbuild, opt, name + '.hmm', self.alns[name]])
-            except (subprocess.CalledProcessError) as exception:
-                print("Error running {}: ".format(
-                    self.hmmbuild) + str(exception))
-
-            self.hmms[name] = name + '.hmm'
+    def write(self):
+        for seq in self.bed:
+            with open(seq + '.bed', 'w') as out:
+                for line in self.bed[seq]:
+                    out.write('\t'.join(line) + '\n')
 
 
 if __name__ == "__main__":
