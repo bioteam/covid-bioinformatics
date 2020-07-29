691c6826d6cc17803cada233574406958fe79c96
Brian Osborne <bosborne@c100-002.frontera.tacc.utexas.edu>
2020-07-29 08:19:03 -0500

    Add vars.py to setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     scripts=['covid_bio/download_gb_by_taxid.py', 'covid_bio/feature_to_gene_and_protein.py',
              'covid_bio/seqs_to_aligns_and_hmms.py', 'covid_bio/parse_hmmsearch_files.py',
              'covid_bio/make_json.py', 'covid_bio/annotate_to_bed.py', 'covid_bio/cov_dictionary.yaml',
-             'covid_bio/cov_length_variants.yaml'],
+             'covid_bio/cov_length_variants.yaml', 'covid_bio/vars.py'],
     python_requires='>=3.7',
     setup_requires=['setuptools_scm'],
     install_requires=requirements,
