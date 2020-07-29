3108aec9a254268ee250837f829094d8a0578f86
Brian Osborne <briano@bioteam.net>
2020-07-29 17:54:47 -0400

    Documentation on vars

--- covid_bio/vars.py
+++ covid_bio/vars.py
@@ -2,7 +2,11 @@
 
 import os
 
-# Create a default destination directory for all downloaded and created files
+'''
+Create a default destination directory for all downloaded and created files,
+specify email address for Entrez. Adjust as needed.
+'''
+
 COV_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'COV')
 
 EMAIL = 'self@organization.net'
