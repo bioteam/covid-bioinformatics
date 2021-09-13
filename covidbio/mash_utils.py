2c0b7dde0df25b0976e9aefe9257c59cc975881d
Brian Osborne <briano@bioteam.net>
2021-09-13 08:58:59 -0400

    Add test dir and test script, test for make_nr

--- covidbio/mash_utils.py
+++ covidbio/mash_utils.py
@@ -8,11 +8,11 @@ from Bio import SeqIO
 '''
 Use the mash application to do pairwise sequence comparision using 
 kmers, and use similarity data to identify closely related sequences 
-which can be removed to create an "nr" file.
+that are removed to create an "nr" file.
 '''
 def make_nr(fname, informat='fasta', threshold=0.1):
     results = run_mash(fname)
-    # Arbitrarily pick first of sorted pair of ids and ignore sequence compared to itself
+    # Arbitrarily pick first of sorted pair of ids to skip and ignore sequence compared to itself
     skip = set([sorted([e[0], e[1]])[0] for e in results if float(e[2]) < threshold and e[0] != e[1]])
     # For example, input is viruses.dat, output is viruses-nr.fa
     with open(os.path.basename(fname).split('.')[0] + '-nr.fa', 'w') as out:
@@ -24,7 +24,6 @@ def make_nr(fname, informat='fasta', threshold=0.1):
 mash results example:
 
 [['CYS1_DICDI', 'CYS1_DICDI', '0', '0', '335/335'], 
-['ALEU_HORVU', 'CYS1_DICDI', '0.649122', '2.36664e-07', '1/688'], 
 ['CATH_HUMAN', 'CYS1_DICDI', '0.567663', '2.38275e-14', '2/660'], 
 ['CATL_HUMAN', 'CATH_HUMAN', '1', '1', '0/652'],
 ['CATH_HUMAN', 'CATH_RAT', '0.120713', '0', '110/542']]
