1db5f9fab3e3c7f1eed224e5f36408b7d1dc21ca
Brian Osborne <bosborne@alum.mit.edu>
2021-10-06 11:42:37 -0400

    Change shell script to use argparse

--- process-dataset/example.sh
+++ process-dataset/example.sh
@@ -2,4 +2,4 @@
 
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 gunzip uniprot_sprot.fasta.gz
-python3 generate_dataset.py uniprot_sprot.fasta ../../DEEPred/TrainTestDatasets/ ctriad ./
+python3 generate_dataset.py -fasta_file_path uniprot_sprot.fasta -go_annots_path ../../DEEPred/TrainTestDatasets/ -output_dir .
