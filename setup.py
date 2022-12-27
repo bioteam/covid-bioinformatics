830548c1b7d9552014927214d967ffed3c38fde5
Brian Osborne <bosborne@alum.mit.edu>
2022-12-27 13:36:29 -0500

    Add MANIFEST for YAML files

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
     scripts=['covidbio/download_gb_by_taxid.py', 'covidbio/feature_to_gene_and_protein.py',
              'covidbio/seqs_to_aligns_and_hmms.py', 'covidbio/parse_hmmsearch_files.py',
              'covidbio/fast_uniprot_parser.py', 'covidbio/annotate_to_bed.py',
-             'covidbio/config.yaml', 'covidbio/cov_features.yaml', 'covidbio/cov_length_variants.yaml',
-             'covidbio/cov_strains.yaml', 'covidbio/utilities.py'],
+             'covidbio/utilities.py', 'covidbio/mash_utils.py', 'covidbio/swiss-to-fasta.py'],
     python_requires='>=3.7',
     setup_requires=['setuptools_scm'],
     install_requires=requirements,
@@ -21,5 +20,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: Apache Software License"
-    ]
+    ],
+    include_package_data=True
 )
